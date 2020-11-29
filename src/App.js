4a712e2e56e01665e127fc9423ee6388e837f7e4
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-29 11:44:15 -0700

    projects have been component-ified

--- src/App.js
+++ src/App.js
@@ -4,39 +4,46 @@ function App() {
   return (
     <div>
       <header>
-        <h1>My Boppin' Bio</h1>
+        <h1>Boggs Portfolio</h1>
       </header>
   
       <div className="container-fluid">
         <section className="row">
-          <Project />
-          <div className="col-6-md col-12 projects">
-            <h3>Eat Da Burger</h3>
-            <p><a href="https://intense-spire-84318.herokuapp.com/">Heroku link</a></p>
-            <p><a href="https://github.com/Matt-Boggs/burger">GitHub Repository</a></p>
-            <p>This application uses handlebars, routing, and ORM with a CMV structure, to create and add to a database of burgers, The user is also able to click a button to "devour" a burger, as well as a button to delete a burger with "YUCK"</p>
-            <img src="./images/burger.PNG"/>
-          </div>
-          <div className="col-6-md col-12 projects">
-              <h3>What's the Weather Like?</h3>
-              <p><a href="https://matt-boggs.github.io/Whats-the-Weather-Like/">GitHub Pages</a></p>
-              <p><a href="https://github.com/Matt-Boggs/Whats-the-Weather-Like">GitHub Repository</a></p>
-              <p>After the user enters a city to search for, shows the temperature, humidity, windspeed and UV index for that city. Also shows the 5 day forecast for that city. Uses three different weather-based API's</p>
-              <img src="../images/Weather.PNG"/>
-          </div>
-          <div className="col-6-md col-12 projects">
-              <h3>Let's Hit the Slopes</h3>
-              <p><a href="https://matt-boggs.github.io/Lets-Hit-the-Slopes/">GitHub Pages</a></p>
-              <p><a href="https://github.com/Matt-Boggs/Lets-Hit-the-Slopes">GitHub Repository</a></p>
-              <p>This app shows the user the base and peak conditions at Pila ski resort in Italy. Also accepts user input to convert ski pass prices to user's chosen currency. Uses 2 API's</p>
-              <img src="../images/Slopes.PNG"/>
-          </div>
-          <div className="col-6-md col-12 projects">
-              <h3>Who Works Here?</h3>
-              <p><a href= "https://github.com/Matt-Boggs/Who-Works-Here" >GitHub Pages</a></p>
-              <p>This app takes in user input for different kind of employees, and then uses that data to generate an html with cards for each team member</p>
-              <img src="https://i.imgur.com/Wi7WI5z.png"/>
-          </div>
+          <Project
+            title="Shelf Indulgence"
+            hosted="https://shelf-indulgence-bc.herokuapp.com/"
+            repo="https://github.com/MarkBrazee/project2_BookClub"
+            about="Search for books using an api, add those books to a database of books you want to read, and keep track of books you have read"
+            image="./images/Shelf.png"
+          />
+          <Project
+            title="Eat Da Burger"
+            hosted="https://intense-spire-84318.herokuapp.com/"
+            repo="https://github.com/Matt-Boggs/burger"
+            about="This application uses handlebars, routing, and ORM with a CMV structure, to create and add to a database of burgers, The user is also able to click a button to 'devour' a burger, as well as a button to delete a burger with 'YUCK'"
+            image="./images/burger.PNG"
+          />
+          <Project
+            title="What's the Weather Like?"
+            hosted="https://matt-boggs.github.io/Whats-the-Weather-Like/"
+            repo="https://github.com/Matt-Boggs/Whats-the-Weather-Like"
+            about="After the user enters a city to search for, shows the temperature, humidity, windspeed and UV index for that city. Also shows the 5 day forecast for that city. Uses three different weather-based API's"
+            image="./images/Weather.PNG"
+          />
+          <Project
+            title="Let's Hit the Slopes"
+            hosted="https://matt-boggs.github.io/Lets-Hit-the-Slopes/"
+            repo="https://github.com/Matt-Boggs/Lets-Hit-the-Slopes"
+            about="This app shows the user the base and peak conditions at Pila ski resort in Italy. Also accepts user input to convert ski pass prices to user's chosen currency. Uses 2 API's"
+            image="./images/Slopes.PNG"
+          />
+          <Project
+            title="Who Works Here?"
+            hosted="https://github.com/Matt-Boggs/Who-Works-Here"
+            repo="https://github.com/Matt-Boggs/Who-Works-Here"
+            about="This app takes in user input for different kind of employees, and then uses that data to generate an html with cards for each team member"
+            image="https://i.imgur.com/Wi7WI5z.png"
+          />
         </section>
       </div>
     </div>
--- src/component/Project.js
+++ src/component/Project.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const Project = (props)=>{
 
-        return (
-            <div className="col-6-md col-12 projects">
-                <h3>Shelf Indulgence</h3>
-                <p><a href="https://shelf-indulgence-bc.herokuapp.com/">Heroku link</a></p>
-                <p><a href="https://github.com/MarkBrazee/project2_BookClub">GitHub Repository</a></p>
-                <p>Search for books using an api, add those books to a database of books you want to read, and keep track of books you have read</p>
-                <img src="../images/Shelf.png"/>
-            </div>
-          )
+    return (
+        <div className="col-6-md col-12 projects">
+            <h3>{props.title}</h3>
+            <p><a href={props.hosted}>Hosted page</a></p>
+            <p><a href={props.repo}>GitHub Repository</a></p>
+            <p>{props.about}</p>
+            <img src={props.image}/>
+        </div>
+    )
 };
 
 export default Project;
