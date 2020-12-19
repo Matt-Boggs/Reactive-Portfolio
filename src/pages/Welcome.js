53f10a72583bf85f1b842d1ea65eb8319944790a
Matt Boggs <matt.david.boggs@gmail.com>
2020-12-19 15:07:34 -0700

    more styling on the welcome cards

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -5,29 +5,29 @@ const Welcome = ()=>{
         <div className="welcome">
             <div className="container">
                 <div className="row">
-                    <div className="card shadow-lg col-3">
-                        <img className="card-img-top" src="./images/gitThumb.PNG" alt="Card image"/>
-                        <div className="card-body">
-                            <a href="https://github.com/Matt-Boggs" className="btn btn-primary stretched-link" target="_blank" rel="noreferrer noopener">Github Profile</a>
-                        </div>
+                    <div className="card shadow-lg col-10">
+                        <a href="https://giftr-registry.herokuapp.com/" className="card-header stretched-link" target="_blank" rel="noreferrer noopener">Current Project</a>
+                        <img className="card-img" src="./images/giftHome.PNG" alt="Card image"/>
+                        {/* <div className="card-body">
+                        </div> */}
                     </div>
-                    <div className="card shadow-lg col-3">
-                        <img className="card-img-top" src="./images/giftHome.PNG" alt="Card image"/>
-                        <div className="card-body">
-                        <a href="https://giftr-registry.herokuapp.com/" className="btn btn-primary stretched-link" target="_blank" rel="noreferrer noopener">Current Project</a>
-                        </div>
+                    <div className="card shadow-lg col-5">
+                        <a href="https://github.com/Matt-Boggs" className="card-header stretched-link" target="_blank" rel="noreferrer noopener">Github Profile</a>
+                        <img className="card-img" src="./images/gitThumb.PNG" alt="Card image"/>
+                        {/* <div className="card-body">
+                        </div> */}
                     </div>
-                    <div className="card shadow-lg col-3">
-                        <img className="card-img-top" src="./images/lnkdnThumb.PNG" alt="Card image"/>
-                        <div className="card-body">
-                        <a href="https://www.linkedin.com/in/matt-david-boggs/" className="btn btn-primary stretched-link" target="_blank" rel="noreferrer noopener">Linkedin Profile</a>
-                        </div>
+                    <div className="card shadow-lg col-5">
+                        <a href="https://www.linkedin.com/in/matt-david-boggs/" className="card-header stretched-link" target="_blank" rel="noreferrer noopener">Linkedin Profile</a>
+                        <img className="card-img" src="./images/lnkdnThumb.PNG" alt="Card image"/>
+                        {/* <div className="card-body">
+                        </div> */}
                     </div>
-                    <div className="card shadow col-12">
+                    {/* <div className="card shadow col-12">
                         <div className="card-body">
                             <a href="/projects" className="btn btn-primary stretched-link">Continue to Portfolio</a> 
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
