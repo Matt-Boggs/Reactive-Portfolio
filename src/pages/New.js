124753acec1226015753d70895b22ec852683e10
Matt Boggs <matt.david.boggs@gmail.com>
2021-01-23 17:34:40 -0700

    new start for landing page, next images as buttons with overlay

---
+++ src/pages/New.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const New = ()=>{
+
+
+
+
+    return (
+        // <div className="container">
+            <div className="row">
+
+
+                
+                <div className="col-12 flex-container">
+                    <img src="https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png" className="rounded-circle leftChoose shadow"/>
+                    <img src="https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png" className="rounded-circle mainChoose shadow-lg"/>
+                    <img src="https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png" className="rounded-circle rightChoose shadow"/>
+                </div>
+                
+
+                
+            </div>
+        // </div>
+    )
+
+
+
+}
+
+export default New;
