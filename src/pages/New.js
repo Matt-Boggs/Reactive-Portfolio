24e25bb356cb5ecf6339dccb818bd9751992cb7a
Matt Boggs <matt.david.boggs@gmail.com>
2021-01-24 09:07:08 -0700

    clickable images, preliminary text placement

--- src/pages/New.js
+++ src/pages/New.js
@@ -12,9 +12,33 @@ const New = ()=>{
 
                 
                 <div className="col-12 flex-container">
-                    <img src="https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png" className="rounded-circle leftChoose shadow"/>
-                    <img src="https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png" className="rounded-circle mainChoose shadow-lg"/>
-                    <img src="https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png" className="rounded-circle rightChoose shadow"/>
+                    <a className="left" href="https://en.wiktionary.org/wiki/%C3%BEegja_%C3%BEunnu_hlj%C3%B3%C3%B0i">
+                        <img src="https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png" className="rounded-circle leftChoose shadow"/>
+                    </a>
+
+                    <a className="middle" href="https://en.wiktionary.org/wiki/%C3%BEegja_%C3%BEunnu_hlj%C3%B3%C3%B0i">
+                        <img src="https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png" className="rounded-circle mainChoose shadow-lg"/>
+                    </a>
+
+                    <a className="right" href="https://en.wiktionary.org/wiki/%C3%BEegja_%C3%BEunnu_hlj%C3%B3%C3%B0i">
+                        <img src="https://static.scientificamerican.com/sciam/cache/file/7A715AD8-449D-4B5A-ABA2C5D92D9B5A21_source.png" className="rounded-circle rightChoose shadow"/>
+                    </a>
+                </div>
+
+                <div className="col-4 flex-container">
+                    <p>
+                        If you’re building React applications, you’re likely sharing some kind of data between React components. When the application scales and sharing that data gets more complex, it’s time to use a state-management library.
+                    </p>
+                </div>
+                <div className="col-4 flex-container">
+                    <p>
+                        If you’re building React applications, you’re likely sharing some kind of data between React components. When the application scales and sharing that data gets more complex, it’s time to use a state-management library.
+                    </p>
+                </div>
+                <div className="col-4 flex-container">
+                    <p>
+                        If you’re building React applications, you’re likely sharing some kind of data between React components. When the application scales and sharing that data gets more complex, it’s time to use a state-management library.
+                    </p>
                 </div>
                 
 
